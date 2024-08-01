a21bce9f184a6384ca3fadbd819c5de5036ea828
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-08-01 20:37:49 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,18 @@
+# Create a temporary directory
+$tempFolder = Join-Path $env:TEMP "WinGetInstall"
+New-Item -ItemType Directory -Force -Path $tempFolder
+
+# Download the latest version of Winget
+$latestWingetMsixBundleUri = "https://aka.ms/getwinget"
+$latestWingetMsixBundle = Join-Path $tempFolder "Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+Invoke-WebRequest -Uri $latestWingetMsixBundleUri -OutFile $latestWingetMsixBundle
+
+# Install Winget
+Add-AppxPackage -Path $latestWingetMsixBundle
+
+# Clean up
+Remove-Item $tempFolder -Recurse
+
 winget install -e --id Opera.Opera Discord.Discord VideoLAN.VLC Foxit.FoxitReader TheDocumentFoundation.LibreOffice 7zip.7zip --accept-package-agreements --accept-source-agreements
 Install-Module PSWindowsUpdate -Force
 Get-WindowsUpdate
