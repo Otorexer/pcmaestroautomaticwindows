aa135cdf150f86247b31e6a9409bc7a257d92d06
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-08-01 20:02:17 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,4 +1,4 @@
+winget install -e --id Opera.Opera Discord.Discord VideoLAN.VLC Foxit.FoxitReader TheDocumentFoundation.LibreOffice 7zip.7zip --accept-package-agreements --accept-source-agreements
 Install-Module PSWindowsUpdate -Force
 Get-WindowsUpdate
 Install-WindowsUpdate -AcceptAll
-winget install -e --id Opera.Opera Discord.Discord VideoLAN.VLC Foxit.FoxitReader TheDocumentFoundation.LibreOffice 7zip.7zip --accept-package-agreements --accept-source-agreements
