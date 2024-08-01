9cb5f923e096f82a3ee291c7cd66423570c2f49c
Otorexer <118485801+Otorexer@users.noreply.github.com>
2024-08-01 20:01:52 +0100

    Update script.ps1

--- script.ps1
+++ script.ps1
@@ -1,3 +1,4 @@
 Install-Module PSWindowsUpdate -Force
 Get-WindowsUpdate
 Install-WindowsUpdate -AcceptAll
+winget install -e --id Opera.Opera Discord.Discord VideoLAN.VLC Foxit.FoxitReader TheDocumentFoundation.LibreOffice 7zip.7zip --accept-package-agreements --accept-source-agreements
